18fb6f36f8d0d76e35892643d112ee76b8b8007f
Erblina Jakupi <higo5t@INF.ELTE.HU>
2021-03-24 23:13:06 +0100

    still

--- expert_data_dashboard/data_dashboard/views.py
+++ expert_data_dashboard/data_dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UploadLinkForm, ColumnsForm
 import csv
 import json
+import urllib.request
 
 from io import StringIO
 
@@ -12,8 +13,8 @@ def index(request):
     context['form'] = form
     if request.POST:
         temp = request.POST.get('link_field', False)
-        print(temp)
-        data = json.load(temp)
+        response = urllib.request.urlopen(temp)
+        data = json.loads(response.read())
         print(data)
         return redirect(result)
     return render(request, "index.html", context)
