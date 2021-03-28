d9790ba51738add13e93f63b1316f2c66f6d84ce
Erblina Jakupi <higo5t@INF.ELTE.HU>
2021-03-28 16:48:39 +0200

    woringon2

--- expert_data_dashboard/data_dashboard/forms.py
+++ expert_data_dashboard/data_dashboard/forms.py
@@ -1,8 +1,6 @@
-from _ast import arg
-
 from django import forms
 from django.forms import URLInput
-from . import views
+
 
 class UploadLinkForm(forms.Form):
      link_field =  forms.URLField(label='Link:')
@@ -30,10 +28,3 @@ VIZUALIZATION_CHOICES = (
 class ColumnsForm(forms.Form):
      columns_field = forms.ChoiceField(label='Columns name:', choices=COLUMNS_CHOICES)
      vizualization_field = forms.ChoiceField(label='Vizualization style:', choices=VIZUALIZATION_CHOICES)
-
-     def __init__(self, *args, **kwargs):
-          super(ColumnsForm, self).__init__(*arg, **kwargs)
-
-          # get the choices from where you need
-          choices = ()
-          self.fields['vote'].choices = choices
